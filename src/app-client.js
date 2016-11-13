8922af192627a1a2ea7ab1d0475340a0bdb7cbe2
Hamza Sheikh <hsheikhm@gmail.com>
2016-11-13 21:51:51 +0000

    created app-client.js

---
+++ src/app-client.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from './components/AppRoutes';
+
+window.onload = () => {
+  ReactDOM.render(
+    <AppRoutes />,
+    document.getElementById('main')
+  );
+};
